050c1cf4ce58a9c1900ff1c575f231422f0d1e9f
mssfr <mssfr1998@gmail.com>
2021-12-03 18:18:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="col-md-12 d-flex">
+       <div className="row">
+<div className="col-md-4 bg-danger">
+<label>dfaf</label>
+</div>
+<div className="col-md-4 bg-danger">
+<label>dfaf</label>
+
+</div>
+<div className="col-md-4 bg-danger">
+<label>dfaf</label>
+
+</div>
+       </div>
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
